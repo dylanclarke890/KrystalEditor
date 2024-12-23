a889c43efacbd2a0f808138fcafb4bf5defa81fc
dylanclarke890 <dylanclarke890@gmail.com>
2024-12-23 00:30:49 +0000

    testing huffman encoding/decoding

--- src/KrystalEditor.cpp
+++ src/KrystalEditor.cpp
@@ -1,6 +1,12 @@
 #include "KrystalEditor.hpp"
 #include <Core/Debug/Macros.hpp>
 #include <Core/Events/Input/KeyboardEvent.hpp>
+#include <IO/BinaryFileReader.hpp>
+#include <IO/BinaryFileWriter.hpp>
+#include <IO/Compression/HuffmanDecoder.hpp>
+#include <IO/Compression/HuffmanEncoder.hpp>
+#include <IO/Compression/HuffmanTree.hpp>
+#include <IO/IO.hpp>
 
 namespace Krys
 {
@@ -13,6 +19,16 @@ namespace Krys
         KRYS_LOG("key pressed %d", event.GetKey());
         return false;
       });
+
+    using encoder_t = IO::HuffmanEncoder<uint16, Endian::Type::System>;
+    using decoder_t = IO::HuffmanDecoder<uint16, Endian::Type::System>;
+
+    encoder_t encoder("./input.txt", "./encoded.txt");
+    encoder.Encode();
+    decoder_t decoder("./encoded.txt", "./decoded.txt", encoder.GetTree());
+    decoder.Decode();
+
+    _running = false;
   }
 
   void KrystalEditor::Update(float) noexcept
