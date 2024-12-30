63344296933c6db3d231a927f5cbe46abb28091b
dylanclarke890 <dylanclarke890@gmail.com>
2024-12-30 04:13:53 +0000

    setup testing for parser

--- src/KrystalEditor.cpp
+++ src/KrystalEditor.cpp
@@ -1,13 +1,11 @@
 #include "KrystalEditor.hpp"
 #include <Core/Debug/Macros.hpp>
 #include <Core/Events/Input/KeyboardEvent.hpp>
-#include <IO/Compression/Huffman.hpp>
-#include <IO/Compression/RLE.hpp>
-#include <IO/DataFlow.hpp>
 #include <IO/IO.hpp>
 #include <IO/Readers/BinaryFileReader.hpp>
 #include <IO/Readers/MemoryReader.hpp>
 #include <IO/Writers/BinaryFileWriter.hpp>
+#include <Scripting/Parser.hpp>
 
 namespace Krys
 {
@@ -21,19 +19,13 @@ namespace Krys
         return false;
       });
 
-    using data_src_t = IO::MemoryReader<Endian::Type::System, Endian::Type::System>;
+    using data_src_t = IO::BinaryFileReader<Endian::Type::System, Endian::Type::System>;
     using data_dst_t = IO::BinaryFileWriter<Endian::Type::System, Endian::Type::System>;
-    using data_flow_t = IO::DataFlow<data_src_t, data_dst_t>;
 
-    using identity_t = IO::Stage::Identity<List<byte>>;
-
-    List<byte> sampleData = {byte {72}, byte {101}, byte {108}, byte {108}, byte {111}, byte {32},
-                             byte {87}, byte {111}, byte {114}, byte {108}, byte {100}};
-    auto input = data_src_t(sampleData);
-    auto output = data_dst_t("./compress/decoded.txt");
-
-    auto pipeline = data_flow_t(&input, &output) | identity_t {};
-    pipeline.Execute();
+    data_src_t src {"./compress/input.txt"};
+    data_dst_t dst {"./compress/decoded.txt"};
+    Lang::Parser parser(src, dst);
+    parser.Parse();
 
     _running = false;
   }
